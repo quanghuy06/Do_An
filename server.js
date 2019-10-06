90865cdad2a532e22cc96daaf57605b088f8c1b1
Huy Tran Quang <huy.tranquang@toshiba-tsdv.com>
2019-10-06 23:26:23 +0700

    edit database
    Signed-off-by: Huy Tran Quang <huy.tranquang@toshiba-tsdv.com>

--- server.js
+++ server.js
@@ -123,8 +123,11 @@ function push_data(){
 				var m_time = value.Date_and_Time.toString().slice(4,24);
 				console.log(value.CO);
 				CO = CO + value.CO;
+
 				//io.sockets.emit('temp', {time:m_time, P2_5:value.P2_5, hum:value.Humidity,CO:value.CO, SO2:value.SO2, temp:value.Temperature});
 			});
+			console.log(CO)
+			CO = 0;
 			console.log(CO)
 			
 	});
